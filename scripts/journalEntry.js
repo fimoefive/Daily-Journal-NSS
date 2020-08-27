6e94d56d86e412e3e98d5e684bf10f0ac7189e5a
fimoefive <brothermj@hotmail.com>
2020-08-26 22:32:37 -0500

    journal entry component display

--- scripts/journalEntry.js
+++ scripts/journalEntry.js
@@ -1,3 +1,5 @@
+import { useJournalEntries } from "./journalDataProvider"
+
 /*
  *  Purpose: To render a single journal entry as an
  *           HTML representation of the data
@@ -5,6 +7,10 @@
 export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
+        <h3>${journal.date}</h3>
+        ${journal.concept}
+        ${journal.entry}
+        ${journal.mood}
             Display the entry's full text, and the date
             it was entered here.
         </section>
